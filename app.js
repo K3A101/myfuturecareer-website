5756c7ad34fb86843b188a3f615f05db4c4fecc3
K3A101 <alexanderkeisha9@gmail.com>
2024-10-05 10:36:58 +0200

    Connecting supabase to the website

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-
-
 require("dotenv").config();
+const { createClient } = require('@supabase/supabase-js') 
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
 const express = require('express');
 const app = express();
 const hostname = "127.0.0.1"
 const PORT = process.env.PORT || 3043;
 const router = require('./routes/route');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 app.use(express.static('public'));
--- routes/route.js
+++ routes/route.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
+const { createClient } = require('@supabase/supabase-js')
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
 router.get('/', (req, res) => {
     res.render('index', {
         title: "My Future Career | Adviesbureau"
     })
+})
+
+router.get('/services', async (req, res) => {
+    const { data, error } = await supabase
+        .from('services')
+        .select()
+        res.send(data);
 })
 
 module.exports = router;
